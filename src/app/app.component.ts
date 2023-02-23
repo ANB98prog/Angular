e5a33d5561489f5a873ba3a7b4cea28a3d6499db
Alex Newman <alex.newman.prog@gmail.com>
2023-02-23 22:01:21 +0600

    Added items dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
+class Item {
+  purchase: string;
+  done: boolean;
+  price: number;
+
+  constructor(purchase: string, price: number){
+    this.purchase = purchase;
+    this.price = price;
+    this.done = false;
+  }
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'shop-app';
+  text: string = "";
+  price: number = 0;
+
+  items: Item[] =
+  [
+    { purchase: "Хлеб", done: false, price: 15.9 },
+    { purchase: "Сосиска в тесте", done: false, price: 17.5 },
+    { purchase: "Кола", done: false, price: 25 },
+  ];
+
+  addItem(text: string, price: number): void {
+    if(text == null || text.trim() == "" || price == null)
+      return;
+    this.items.push(new Item(text, price));
+  }
+
+  removeDoneItems(): void {
+    this.items = this.items.filter(item => !item.done);
+  }
 }
